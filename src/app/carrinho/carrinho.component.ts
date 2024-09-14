052c7f2a36b7d5e88ea34185b7c93a196ab03164
Baratoo <kaique_luiz1@hotmail.com>
2024-09-14 14:54:32 -0300

    atualizações do carrinho

--- src/app/carrinho/carrinho.component.ts
+++ src/app/carrinho/carrinho.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CarrinhoService } from '../carrinho.service';
 import { IProdutoCarrinho } from '../produtos';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-carrinho',
@@ -9,18 +10,39 @@ import { IProdutoCarrinho } from '../produtos';
 })
 export class CarrinhoComponent implements OnInit {
   itensCarrinho: IProdutoCarrinho[] = [];
+  valorTotal = 0;
 
   constructor(
     public carrinhoService: CarrinhoService,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
     this.itensCarrinho = this.carrinhoService.obtemCarrinho();
+    this.calculaValorTotal();
   }
 
   removeProdutoCarrinho(produtoID: number){
     this.itensCarrinho = this.itensCarrinho.filter(item => item.id !== produtoID);
     this.carrinhoService.removerProdutoCarrinho(produtoID);
+    this.calculaValorTotal();
+  }
+
+  calculaValorTotal(){
+                                      //"Reduce" percorre os itens, "prev" e "curr" pega o anterior e o atual
+    this.valorTotal = this.itensCarrinho.reduce((prev, curr) => prev + (curr.preco * curr.quantidade),0);
+  }
+
+  removeSeIgualZero(itemCarrinho: any){
+    if(itemCarrinho.quantidade == 0){
+      this.removeProdutoCarrinho(itemCarrinho.id);
+    }
+  }
+
+  comprar(){
+    alert("Itens comprados com sucesso \n Obrigado por comprar em PragasShoop!");
+    this.carrinhoService.limparCarrinho();
+    this.router.navigate(["produtos"]);
   }
 
 }
